3b43d8bbe926cba1e854cfb272be8384d0bb66e7
mario flores <react@gmail.com>
2019-09-02 19:56:31 -0400

    card list

--- src/App.js
+++ src/App.js
@@ -3,19 +3,30 @@ import logo from './logo.svg';
 import './App.css';
 import {Navbar, NavbarBrand} from 'reactstrap';
 import CatalogComponent from './components/CatalogComponent';
+import {ITEMS} from './shared/items';
 
-function App() {
-  return (
-    <div className="App">
-          <Navbar dark color="primary">
-            <div className="container">
-              <NavbarBrand href="/">Sistema de Seguridad Industrial</NavbarBrand>
-            </div>
-          </Navbar>
-          <CatalogComponent></CatalogComponent>
-        </div>
+class App extends React.Component {
 
-  );
+  constructor(props, context) {
+    super(props, context);
+    this.state={
+        items: ITEMS
+    }
+}
+
+  render(){
+    return (
+      <div className="App">
+            <Navbar dark color="primary">
+              <div className="container">
+                <NavbarBrand href="/">Sistema de Seguridad Industrial</NavbarBrand>
+              </div>
+            </Navbar>
+            <CatalogComponent items={this.state.items}></CatalogComponent>
+          </div>
+  
+    );
+  }
 }
 
 export default App;
--- src/components/CatalogComponent.js
+++ src/components/CatalogComponent.js
@@ -1,47 +1,10 @@
 import React, { Component } from 'react';
-import {Media} from 'reactstrap';
+import {Media, Card, CardImg, CardImgOverlay, CardTitle} from 'reactstrap';
 class CatalogComponent extends Component {
 
     constructor(props, context) {
         super(props, context);
     
-        this.state={
-            items:[
-                {
-                    name: 'Helmet',
-                    image: '/assets/images/helmet.jpg',
-                    category: 'safety',
-                    label: 'ultra safe',
-                    price: '4.99',
-                    description: 'Un casco de seguridad útil para proteger en ámbitos como la construcción, fabricas y otros'
-                },
-                {
-                    name: 'Audio protector',
-                    image: '/assets/images/audio.jpg',
-                    category: 'safety',
-                    label: 'noise filter',
-                    price: '12.99',
-                    description: 'Protege de ruidos auditivos por encima de los decibeles considerados aceptables'
-                },
-                {
-                    name: 'Glasses',
-                    image: '/assets/images/glasses.jpg',
-                    category: 'safety',
-                    label: 'New technology',
-                    price: '31.99',
-                    description: 'Permite filtrar rayos ultravioleta, material anti rayas, anti caidas'
-                },
-                {
-                    name: 'Gloves',
-                    image: '/assets/images/gloves.jpg',
-                    category: 'safety',
-                    label: '',
-                    price: '12.99',
-                    description: 'Hechos de algodón con goma antideslizante, especial para industria química.'
-                }
-                
-            ]
-        }
     }
     
 
@@ -52,19 +15,17 @@ class CatalogComponent extends Component {
         };
 
         
-        var catalog= this.state.items.map(item => {
+        var catalog= this.props.items.map(item => {
             return (
-                <div key={item.id} className="col-12 mt-5">
-                    <Media tag="li">
-                        <Media left middle>
-                            <Media object src={item.image} alt={item.name} style={imgStyle} />
-                        </Media>
-                        <Media body className="ml-5">
-                            <Media heading>{item.name}</Media>
-                            <p>{item.description}</p>
-                        </Media>
-                    </Media>
-                </div>
+                <div key={item.id} className="col-12 col-md-5 m-1">
+            <Card>
+                    <CardImg width="100%" src={item.image} alt={item.name}/>
+                <CardImgOverlay>
+                    <CardTitle>{item.name}</CardTitle>
+                </CardImgOverlay>
+            </Card>
+        </div>
+
             );
         });
         
@@ -72,9 +33,9 @@ class CatalogComponent extends Component {
         return (
               <div className="container">
                 <div className="row">
-                    <Media list>
+                    
                         {catalog}
-                    </Media>
+                    
                 </div>
             </div>
 
